f7aa4e748a6a3cf112ce9a7fc820e1cdb6bb7dac
nicoliprogramer <imotivacionesf@gmail.com>
2023-05-22 16:38:43 -0300

    .postFormContainer

--- schemas/userModel.js
+++ schemas/userModel.js
@@ -3,15 +3,18 @@ import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    firstName: { type:String, required: true, trim: true },    
-    lastName: { type:String, required: true, trim: true },
-    username: { type:String, required: true, trim: true, unique: true },
-    email: { type:String, required: true, trim: true, unique: true },
-    password: { type:String, required: true },
-    profilePic: { type:String, default: "/images/profilePic.png" },
-}, {timestamps: true})
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: "/images/profilePic.jpg" },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
-export default User
+export default User;
