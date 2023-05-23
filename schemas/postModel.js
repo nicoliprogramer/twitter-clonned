97fab42aad9c3e5430e6c76bfe854b413bc7b054
nicoliprogramer <imotivacionesf@gmail.com>
2023-05-22 23:02:01 -0300

    posts mongoose schema

---
+++ schemas/postModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    content: { type: String, trim: true },
+    postedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
