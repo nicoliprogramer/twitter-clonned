df8bc943146d472fc7ef88cb1fbd9743452fdf42
nicoliprogramer <imotivacionesf@gmail.com>
2023-05-22 21:44:53 -0300

    adding a click

--- public/js/common.js
+++ public/js/common.js
@@ -4,7 +4,7 @@ $("#postTextarea").keyup((event) => {
 
   let submitButton = $("#submitPostButton");
 
-  //   if (submitButton.length == 0) return alert("no submit button found");
+  if (submitButton.length == 0) return alert("no submit button found");
 
   if (value == "") {
     submitButton.prop("disabled", true);
@@ -12,4 +12,15 @@ $("#postTextarea").keyup((event) => {
   }
 
   submitButton.prop("disabled", false);
+});
+
+$("#submitPostButton").click(() => {
+  let button = $(event.target);
+  let textbox = $("#postTextarea");
+
+  let data = {
+    content: textbox.val(),
+  };
+
+  $.post("/api/post", data, (postData, status, xhr) => {});
 });
