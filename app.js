ceddd889073dfa5163602c2bb21ef9368c5dc4e9
nicoliprogramer <imotivacionesf@gmail.com>
2023-05-26 14:13:04 -0300

    passing the user logged in info to the client

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get("/", requireLogin, (req, res, next) => {
   let payload = {
     pageTitle: "Home",
     userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
   };
 
   res.status(200).render("home", payload);
