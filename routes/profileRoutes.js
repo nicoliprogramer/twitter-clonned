eda8c5536402d670ae8dd79e3f84a722bbcb0789
nicoliprogramer <imotivacionesf@gmail.com>
2023-05-31 11:57:38 -0300

    user not found error message

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,15 +5,28 @@ import bcrypt from "bcrypt";
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  let payload = {
-    pageTitle: req.session.user.username,
-    userLoggedIn: req.session.user,
-    userLoggedInJs: JSON.stringify(req.session.user),
-    profileUser: req.session.user,
-  };
+router.get("/:username", async (req, res, next) => {
+  let payload = await getPayload(req.params.username, req.session.user);
 
   res.status(200).render("profilePag", payload);
 });
+
+async function getPayload(username, userLoggedIn) {
+  let user = await User.findOne({ username: username });
+
+  if (user == null) {
+    return {
+      pageTitle: "User not found",
+      userLoggedIn: userLoggedIn,
+      userLoggedInJs: JSON.stringify(userLoggedIn),
+    };
+  }
+  return {
+    pageTitle: user.username,
+    userLoggedIn: userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  };
+}
 
 export default router;
